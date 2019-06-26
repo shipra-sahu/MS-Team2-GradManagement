745caf387752803d356d4e7a57085adaf2f1a2cf
Shipra Sahu <shipra.sahu@accolite.com>
2019-06-26 12:50:23 +0530

    add-edit modified

--- src/app/gradmanagement/gradmanagement.module.ts
+++ src/app/gradmanagement/gradmanagement.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { GradmanagementRoutingModule } from './gradmanagement-routing.module';
 import { AddEditComponent } from './add-edit/add-edit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AddEditComponent],
   imports: [
     CommonModule,
-    GradmanagementRoutingModule
+    GradmanagementRoutingModule,
+    FormsModule
   ]
 })
 export class GradmanagementModule { }
